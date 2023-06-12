4890a985b66960e947113c3c6fc102288591db71
SeoMiYoung <seo20seo00@naver.com>
2023-06-12 22:40:14 +0900

    IIFE(즉시 실행 함수 표현)을 사용

--- src/app.js
+++ src/app.js
@@ -1,2 +1,4 @@
 // sum함수를 가지고 있는 math모듈을 가지고 있는 app.js
-console.log(sum(1,2));
+
+// sum이라는 함수는 전역 스코프에 없기 때문에, math.sum으로 접근해야함
+console.log(math.sum(1,2));
--- src/math.js
+++ src/math.js
@@ -1,3 +1,12 @@
-function sum(a,b) {
-  return a+b;
-}
+// ||는 첫번째 truthy를 반환한다.
+// 즉, math가 존재하면 math를 할당하고, math가 존재하지 않으면 빈객체를 할당함.
+var math = math || {};
+
+// IIFE (즉시 실행 함수)
+(function() {
+  function sum(a,b) {
+    return a+b;
+  }
+  // 외부에서 쓸 수 있게 전역 space인 math에 할당
+  math.sum = sum;
+})();
