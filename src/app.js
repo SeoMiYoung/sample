a88c793a15e758eb98abc26470e0a4893802b555
SeoMiYoung <seo20seo00@naver.com>
2023-06-13 23:49:23 +0900

    ES2015 표준 모듈 시스템 방식을 사용해서 sum함수 불러오기

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
-// sum함수를 가지고 있는 math모듈을 가지고 있는 app.js
+/*
+import * as math from './math.js'; // [방법1] 모든 export를 math라는 객체로 가져오기
+import { sum } from './math.js'; // [방법2] sum함수만 가져오기
+*/
 
-// sum이라는 함수는 전역 스코프에 없기 때문에, math.sum으로 접근해야함
+import * as math from './math.js';
+
 console.log(math.sum(1,2));
--- src/math.js
+++ src/math.js
@@ -1,12 +1,6 @@
-// ||는 첫번째 truthy를 반환한다.
-// 즉, math가 존재하면 math를 할당하고, math가 존재하지 않으면 빈객체를 할당함.
-var math = math || {};
+// ES2015 표준 모듈 시스템 방식
 
-// IIFE (즉시 실행 함수)
-(function() {
-  function sum(a,b) {
-    return a+b;
-  }
-  // 외부에서 쓸 수 있게 전역 space인 math에 할당
-  math.sum = sum;
-})();
+export function sum(a,b) {
+  return a+b;
+}
+
