2a7bf0fe365470e4b497edc519594224515e688a
SeoMiYoung <seo20seo00@naver.com>
2023-06-15 15:15:16 +0900

    번들링하기

--- src/app.js
+++ src/app.js
@@ -6,3 +6,10 @@ import { sum } from './math.js'; // [방법2] sum함수만 가져오기
 import * as math from './math.js';
 
 console.log(math.sum(1,2));
+
+
+import * as math1 from './math.js'; 
+import * as math2 from './math.js'; 
+
+import { sum } from './math1.js'; 
+import { sum } from './math2.js';
