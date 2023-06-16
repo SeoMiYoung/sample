a5081e0c3a15b3d80c88229f75e3e0206c95af05
SeoMiYoung <seo20seo00@naver.com>
2023-06-16 13:00:05 +0900

    webpack.config.js 파일 구성

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+// webpack.config.js 는 웹팩 설정 파일입니다!
+
+const path = require('path'); 
+
+module.exports = {
+  mode: 'development',
+  entry: { // entry가 여러개일수도 있다
+    main: './src/app.js' // 상대경로
+  },
+  output: {
+    path: path.resolve('./dist'), // path는 절대경로로
+    filename: '[name].js'
+  }
+}
